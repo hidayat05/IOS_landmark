1b1599acfa0ca234054350ef6bcbc44e8374eed2
Nur Hidayat <hidayat.speed78@gmai.com>
2021-04-24 16:44:46 +0700

    Initial Commit

---
+++ Landsmark/LandsmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandsmarkApp.swift
+//  Landsmark
+//
+//  Created by User on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandsmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
